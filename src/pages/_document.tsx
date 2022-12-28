c844b649733456378faeba26408a7bd43e3838b4
Abdul Rahman Al Sbeinaty <abdul@lunapr.io>
2022-12-28 17:28:37 +0400

    add nav progress bar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,14 @@ import { theme } from "../components/theme";
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel={"stylesheet"}
+          href={
+            "https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          }
+        />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
